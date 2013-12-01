eef581dc0590969ec111494b9255ef927b79bca5
galactoise@gmail.com <galactoise@gmail.com>
2013-12-01 13:11:29 -0800

    Adding GET method to client

--- HueClient.js
+++ HueClient.js
@@ -5,6 +5,21 @@ var hueClient = {
 			//define endpoints
 			this.rootEndpoint = "/api/";
 			this.consumerRootEndpoint = "/api/%s";
-			this.lightsByConsumerId = "/api/%s/lights";
+			this.lightsByConsumerIdEndpoint = "/api/%s/lights";
+		},
+
+		
+		get: function(path){
+			var uri = controllerUrl + path;
+			alert(uri);
+			jQuery.ajax({
+				type: GET,
+				dataType: "json",
+				url: uri
+			});
+		},
+		
+		getLightsByConsumerId: function(consumerId){
+			var path = this.lightsByConsumerIdEndpoint.replace("%s",consumerId);
 		}
 }
