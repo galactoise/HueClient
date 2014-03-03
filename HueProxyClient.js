0946dbcf2001ea3f5853267e034ee75f887a6017
galactoise@gmail.com <galactoise@gmail.com>
2014-03-02 19:12:18 -0800

    Updates for drag and drop

--- HueProxyClient.js
+++ HueProxyClient.js
@@ -3,5 +3,17 @@ var hueProxyClient = {
 			this.serviceUrl = serviceUrl;
 			this.rootEndpoint = "/rest/";
 			this.templatesEndpoint = "/rest/templates";
-		}
+		},
+		get: function(path){
+			var uri = this.controllerUrl + path;
+			console.log("uri: " + uri);
+			var request = jQuery.ajax({
+				type: "GET",
+				dataType: "json",
+				url: uri,
+				async: false
+			});
+			
+			return request;
+		},
 };
